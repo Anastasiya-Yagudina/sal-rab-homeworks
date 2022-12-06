6bd170fc908b88263e2325ac611c71f8cc0edd37
unknown <yagudina_a@icloud.com>
2022-12-06 23:50:44 +0600

    Home work2

--- task_2/solution.js
+++ task_2/solution.js
@@ -6,12 +6,23 @@ function calcShipping(sum, min, shipping) {
     // Задание №2.1. Рассчитать доставку
 
     // создайте переменную shippingSum
+    let shippingSum;
+    if (productsSum == 0){
+        shippingSum = 0;
+    }
 
     // если productsSum равно 0,
     // то shippingSum присвоить значение 0
 
+    if (productsSum >= freeShippingMinSum){
+        shippingSum = 0;
+    }
     // если productsSum Больше или равна freeShippingMinSum,
     // то shippingSum присвоить значение 0
+
+    if (productsSum > 0 && productsSum < freeShippingMinSum){
+        shippingSum = shippingPrice;
+    }
 
     // если productsSum больше 0 и меньше freeShippingMinSum,
     // то shippingSum присвоить значение shippingPrice
@@ -28,7 +39,14 @@ function calcDiscount(sum, min, discount) {
 
     // Задание №2.2. Рассчитать скидку
 
+    let discountSum
     // создайте переменную discountSum
+
+    if (productsSum >= discountMinSum){
+        discountSum = productsSum*discountPart/100
+    } else {
+        discountSum=0
+    }
 
     // если productsSum больше или равно discountMinSum,
     // то присвойте discountSum значение discountPart процентов от productsSum,
@@ -47,13 +65,18 @@ function calcInvoice({sum, discountMinSum, discountPart, shippingFreeMinSum, shi
 
     // создайте переменную totalSum
 
+    let totalSum = productsSum - discountSum;
+
     // присвойте totalSum значение productsSum
     // уменьшите totalSum на discountSum
 
     let shippingSum = calcShipping(totalSum, shippingFreeMinSum, shippingPrice); // не изменяйте эту строку!!!
 
+    totalSum = totalSum + shippingSum
     // прибавьте к totalSum значение shippingSum
 
+    let freeShipping
+    freeShipping = shippingSum == 0;
     // создайте переменную freeShipping
     // запишите без использования if или любых других условий:
     // если shippingSum равно нулю, то freeShipping должна быть равна true, иначе freeShipping должна быть равна false
