bb5e9f818b155e1dabbf32d7c62aa1f99a3bd7a3
unknown <yagudina_a@icloud.com>
2022-12-08 23:01:26 +0600

    Apdate

--- task_2/solution.js
+++ task_2/solution.js
@@ -7,16 +7,14 @@ function calcShipping(sum, min, shipping) {
 
     // создайте переменную shippingSum
     let shippingSum;
-    if (productsSum == 0){
+    if (productsSum == 0 || productsSum >= freeShippingMinSum) {
         shippingSum = 0;
-    }
+      } 
 
     // если productsSum равно 0,
     // то shippingSum присвоить значение 0
+    
 
-    if (productsSum >= freeShippingMinSum){
-        shippingSum = 0;
-    }
     // если productsSum Больше или равна freeShippingMinSum,
     // то shippingSum присвоить значение 0
 
@@ -39,14 +37,9 @@ function calcDiscount(sum, min, discount) {
 
     // Задание №2.2. Рассчитать скидку
 
-    let discountSum
     // создайте переменную discountSum
 
-    if (productsSum >= discountMinSum){
-        discountSum = productsSum*discountPart/100
-    } else {
-        discountSum=0
-    }
+   let discountSum = productsSum >= discountMinSum ? productsSum * discountPart / 100 : 0;
 
     // если productsSum больше или равно discountMinSum,
     // то присвойте discountSum значение discountPart процентов от productsSum,
